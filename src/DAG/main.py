72a1e73f465bd884975b2fb2411e53a4628caa77
Anatoly <75267969+zhukovanan@users.noreply.github.com>
2022-06-15 17:56:41 +0300

    Add files via upload

---
+++ src/DAG/main.py
@@ -0,0 +1,83 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from project.plugins.plugin import generate_report, get_report, get_increment, upload_data_to_staging, postgres_conn_id
+from datetime import datetime, timedelta
+
+
+args = {
+    "owner": "student",
+    'email': ['student@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+business_dt = '{{ ds }}'
+
+with DAG(
+        'Customer_retention_dag',
+        default_args=args,
+        description='Dag in response of customer retenetion calculation',
+        catchup=True,
+        start_date=datetime.today() - timedelta(days=8),
+        end_date=datetime.today() - timedelta(days=1),
+) as dag:
+    generate_report = PythonOperator(
+        task_id='generate_report',
+        python_callable=generate_report)
+
+    get_report = PythonOperator(
+        task_id='get_report',
+        python_callable=get_report)
+
+    get_increment = PythonOperator(
+        task_id='get_increment',
+        python_callable=get_increment,
+        op_kwargs={'date': business_dt})
+
+    upload_user_order_inc = PythonOperator(
+        task_id='upload_user_order_inc',
+        python_callable=upload_data_to_staging,
+        op_kwargs={'date': business_dt,
+                   'filename': 'user_orders_log_inc.csv',
+                   'pg_table': 'user_order_log',
+                   'pg_schema': 'staging'})
+
+    update_d_item_table = PostgresOperator(
+        task_id='update_d_item',
+        postgres_conn_id=postgres_conn_id,
+        sql="sql/mart.d_item.sql")
+
+    update_d_customer_table = PostgresOperator(
+        task_id='update_d_customer',
+        postgres_conn_id=postgres_conn_id,
+        sql="sql/mart.d_customer.sql")
+
+    update_d_city_table = PostgresOperator(
+        task_id='update_d_city',
+        postgres_conn_id=postgres_conn_id,
+        sql="sql/mart.d_city.sql")
+
+    update_f_sales = PostgresOperator(
+        task_id='update_f_sales',
+        postgres_conn_id=postgres_conn_id,
+        sql="sql/mart.f_sales.sql",
+        parameters={"date": {business_dt}}
+    )
+
+    update_f_customer_retention = PostgresOperator(
+        task_id='update_f_customer_retention',
+        postgres_conn_id=postgres_conn_id,
+        sql="sql/mart.f_customer_retention.sql"
+    )
+
+
+    (
+            generate_report
+            >> get_report
+            >> get_increment
+            >> upload_user_order_inc
+            >> [update_d_item_table, update_d_city_table, update_d_customer_table]
+            >> update_f_sales >> update_f_customer_retention
+    )
