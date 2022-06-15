dd7395b6949ea5a54e6fb077a36f1a917e4441c8
Anatoly <75267969+zhukovanan@users.noreply.github.com>
2022-06-15 18:03:15 +0300

    Add files via upload

---
+++ src/DAG/plugins/plugin.py
@@ -0,0 +1,94 @@
+import time
+import requests
+import json
+import pandas as pd
+
+from airflow.hooks.http_hook import HttpHook
+from airflow.hooks.base import BaseHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+http_conn_id = HttpHook.get_connection('http_conn_id')
+api_key = http_conn_id.extra_dejson.get('api_key')
+base_url = http_conn_id.host
+
+postgres_conn_id = 'postgresql_de'
+
+nickname = 'zhukov-an-an'
+cohort = '1'
+headers = {
+    'X-Nickname': nickname,
+    'X-Cohort': cohort,
+    'X-Project': 'True',
+    'X-API-KEY': api_key,
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+
+def generate_report(ti):
+    print('Making request generate_report')
+
+    response = requests.post(f'{base_url}/generate_report', headers=headers)
+    response.raise_for_status()
+    task_id = json.loads(response.content)['task_id']
+    ti.xcom_push(key='task_id', value=task_id)
+    print(f'Response is {response.content}')
+
+
+def get_report(ti):
+    print('Making request get_report')
+    task_id = ti.xcom_pull(key='task_id')
+
+    report_id = None
+
+    for i in range(6):
+        response = requests.get(f'{base_url}/get_report?task_id={task_id}', headers=headers)
+        response.raise_for_status()
+        print(f'Response is {response.content}')
+        status = json.loads(response.content)['status']
+        if status == 'SUCCESS':
+            report_id = json.loads(response.content)['data']['report_id']
+            break
+        else:
+            time.sleep(10)
+
+    if not report_id:
+        raise TimeoutError()
+
+    ti.xcom_push(key='report_id', value=report_id)
+    print(f'Report_id={report_id}')
+
+
+def get_increment(date, ti):
+    print('Making request get_increment')
+    report_id = ti.xcom_pull(key='report_id')
+    response = requests.get(
+        f'{base_url}/get_increment?report_id={report_id}&date={str(date)}T00:00:00',
+        headers=headers)
+    response.raise_for_status()
+    print(f'Response is {response.content}')
+
+    increment_id = json.loads(response.content)['data']['increment_id']
+    ti.xcom_push(key='increment_id', value=increment_id)
+    print(f'increment_id={increment_id}')
+
+
+def upload_data_to_staging(filename, date, pg_table, pg_schema, ti):
+    increment_id = ti.xcom_pull(key='increment_id')
+    s3_filename = f'https://storage.yandexcloud.net/s3-sprint3/cohort_{cohort}/{nickname}/project/{increment_id}/{filename}'
+
+    local_filename = date.replace('-', '') + '_' + filename
+
+    response = requests.get(s3_filename)
+    open(f"{local_filename}", "wb").write(response.content)
+
+    df = pd.read_csv(local_filename)
+    df.drop_duplicates(subset=['id'])
+    df.drop('id', inplace=True, axis=1)
+
+    if 'status' not in df.columns:
+        df['status'] = 'shipped'
+
+    postgres_hook = PostgresHook(postgres_conn_id)
+    engine = postgres_hook.get_sqlalchemy_engine()
+    df.to_sql(pg_table, engine, schema=pg_schema, if_exists='append', index=False)
